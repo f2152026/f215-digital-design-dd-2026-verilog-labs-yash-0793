9fb4519861b0b580bdefc24d3355d0ba4f519f73
Yash-0793 <f20250793@pilani.bits-pilani.ac.in>
2026-08-28 09:30:26 +0000

    lab01 task4

--- labs/lab01/task4/cla4.v
+++ labs/lab01/task4/cla4.v
@@ -61,6 +61,7 @@ module cla4(
   xor #(2) (sum[1], p1, c1);
   xor #(2) (sum[2], p2, c2);
   xor #(2) (sum[3], p3, c3);
+  
 
   // TODO: your gate-level P/G, carry, and sum logic goes here.
   // (cout should be connected to c4.) Remember the delay on every gate.
